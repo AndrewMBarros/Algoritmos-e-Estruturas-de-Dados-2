cb132f8e308561f49bea606ecd4a5ea775a77d5f
Sidney S. dos Santos Junior <43341605+SidneySantosJr@users.noreply.github.com>
2025-07-02 01:01:53 -0300

    Update palindromos.py

--- Trabalho Final/palindromos.py
+++ Trabalho Final/palindromos.py
@@ -31,3 +31,30 @@ def min_palindrome_cuts(s):
 
     # Marca início do tempo de DP
     t2 = time.time()
+
+cuts = [0] * n
+    path = [[] for _ in range(n)]  # guarda partições
+
+    #O(n²)
+    for i in range(n):
+      #verifica se s[0...i] é um palindromo
+        if is_palin[0][i]:
+            cuts[i] = 0
+            path[i] = [s[0:i+1]]
+        else:
+
+            #maior numero de cortes
+            cuts[i] = i
+
+            #verifica se s[1...i] tem algum corte melhor
+            for j in range(1, i + 1):
+                if is_palin[j][i] and cuts[j - 1] + 1 < cuts[i]:
+                    # 0 1 2 ... j-1 | j ... i
+                    cuts[i] = cuts[j - 1] + 1
+                    path[i] = path[j - 1] + [s[j:i + 1]]
+
+    t3 = time.time()
+    dp_time = t3 - t2
+    total_time = t3 - t0
+
+    return cuts[-1], path[-1], preproc_time, dp_time, total_time
